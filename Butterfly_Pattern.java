f0c1abdace8d043620ab7d321ebcd6f617079ede
Bhuvan Raj Patnaik <147475656+bhuvanraj18@users.noreply.github.com>
2024-05-22 11:08:44 +0530

    Update Butterfly_Pattern.java

--- Butterfly_Pattern.java
+++ Butterfly_Pattern.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class FirstClass {
+public class Butterfly_Pattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
