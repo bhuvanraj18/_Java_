7f00494d660dfa7519ceb83795896ae0e22ae690
Bhuvan Raj Patnaik <147475656+bhuvanraj18@users.noreply.github.com>
2024-07-20 14:20:51 +0530

    Clear_bit.java

---
+++ BitManupulation/Clear_bit.java
@@ -0,0 +1,10 @@
+public class Clear_bit {
+    public static void main(String[] args) {
+        int n = 5;
+        int pos = 2;
+        int bitMask = 1<<pos;
+        int notBitMask = ~(bitMask);
+        int newNumber = notBitMask & n;
+        System.out.println(newNumber);
+    }
+}
