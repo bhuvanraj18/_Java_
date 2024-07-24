164029e856b057890dac2a8e4e35da66b2b6881c
Bhuvan Raj Patnaik <147475656+bhuvanraj18@users.noreply.github.com>
2024-07-24 12:02:11 +0530

    Add files via upload

---
+++ Recursion/first_last_occr_str.java
@@ -0,0 +1,26 @@
+public class first_last_occr_str {
+    public static int first = -1;
+    public static int last = -1;
+
+    public static void findOccurance(String str, int idx, char element) {
+        if (idx == str.length()) {
+            System.out.println("First Occurance is At index:" + first);
+            System.out.println("Last Occurance is At index:" + last);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if (currChar == element) {
+            if (first == -1) {
+                first = idx;
+            } else {
+                last = idx;
+            }
+        }
+        findOccurance(str, idx + 1, element);
+    }
+
+    public static void main(String[] args) {
+        String str = "abaacdaefaah";
+        findOccurance(str, 0, 'a');
+    }
+}
